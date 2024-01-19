c93883a6aeda6864c267bbfb9b2fa5faf02befcb
Tanagends <tanagends@gmail.com>
2024-01-20 01:27:25 +0200

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 #!/usr/bin/pup
 # Installs flask
 
-package { 'Flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
