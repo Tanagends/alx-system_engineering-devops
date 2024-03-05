1e60d249aeb506fd20f459db96a8747599133776
Tanagends <tanagends@gmail.com>
2024-03-05 10:56:09 -0800

    API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and returns the number of
+subscribers for a given subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    if results is None:
+        return 0
+    return results.get("subscribers")
